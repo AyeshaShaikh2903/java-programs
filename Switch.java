5f5ef256a5fb218fa64e55603c8552d52a691a06
AyeshaShaikh2903 <134134085+AyeshaShaikh2903@users.noreply.github.com>
2023-11-13 07:04:30 -0800

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch{
+public static void main(String args[])
+{
+int n=11;
+switch(n){
+case 1:
+System.out.println("equal to 11");
+break;
+case 11:
+System.out.println("equal to 11");
+break;
+case -3:
+System.out.println("equal to -3");
+break;
+default:
+System.out.println("no match found");
+break;
+}
+}
+}
