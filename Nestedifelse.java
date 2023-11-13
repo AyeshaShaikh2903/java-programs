a6ee25e65d678ea2a1d92136b59cda0deaedf5a7
AyeshaShaikh2903 <134134085+AyeshaShaikh2903@users.noreply.github.com>
2023-11-13 07:00:49 -0800

    Add files via upload

---
+++ Nestedifelse.java
@@ -0,0 +1,15 @@
+public class Nestedifelse{
+ public static void main(String args[]){
+int n=10;
+if(n<12){
+System.out.println("n is less than 12");
+}
+else if(n==12){
+System.out.println("n is 12");
+}
+else
+{
+System.out.println("n is greater than 12");
+}
+}
+}
