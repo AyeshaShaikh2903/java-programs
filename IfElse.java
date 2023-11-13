6290e07616904820c6bc6985c2fb1c6585fc91b5
AyeshaShaikh2903 <134134085+AyeshaShaikh2903@users.noreply.github.com>
2023-11-13 06:58:32 -0800

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse{
+public static void main(String args[]){
+if(3>2)
+{
+System.out.println("3 is greater than 2");
+}
+else
+{
+System.out.println("2 is less than 3");
+}
+}
+}
+
